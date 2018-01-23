e3e046f03f4f8c36980cad32d6fd48aa98127314
yzmaggie2008 <31428439+yzmaggie2008@users.noreply.github.com>
2018-01-22 23:20:52 -0800

    almost done

--- js/app.js
+++ js/app.js
@@ -1,66 +1,66 @@
 // create var for restaurants locations
 var locations = [{
-	    name: 'Power Pot',
+	    name: 'Mandarin Gourmet',
 	    category: 'Party',
 	    coordinates: {
-	    	lat: 37.316194,
-	    	lng: -122.032710
+	    	lat: 37.3244998,
+	    	lng: -122.0326229
 	    }
 	  },
 	  {
-	  	name: 'Sizzling Gourmet',
+	  	name: 'TLT BBQ',
 	  	category: 'Party',
 	  	coordinates: {
-	  		lat: 37.322282,
-	  		lng: -122.014836
+	  		lat: 37.32354,
+	  		lng: -122.029824
 	  	}	
 	  },
 	  {
-	  	name: 'CBI Boiling Fish',
+	  	name: 'Red Hot Wok',
 	  	category: 'Party',
 	  	coordinates: {
-	  		lat: 37.322514,
-	  		lng: -122.017361
+	  		lat: 37.3221066,
+	  		lng: -122.014911
 	  	}
 	  },
 	  {
-	  	name: 'Taiwan Porridge Kingdom',
+	  	name: 'Guocui',
 	  	category:'Alone',
 	  	coordinates: {
-	  		lat: 37.336958,
-	  		lng: -122.040243
+	  		lat: 37.31358,
+	  		lng: -122.032511
 		  }
 	  },
 	  {
-	  	name: 'Shanghai Garden',
+	  	name: 'QQ Noodle',
 	  	category:'Alone',
 	  	coordinates: {
-	  		lat: 37.336975,
-	  		lng: -122.040296
+	  		lat: 37.3106959,
+	  		lng: -122.0242433
 	  	}
 	  },
 	  {
-	  	name: 'Beijin Duck House',
+	  	name: 'Little Sheep Mongolian Hot Pot',
 	  	category: 'Party',
 	  	coordinates: {
-	  		lat: 37.310779,
-	  	    lng: -122.023916
+	  		lat: 37.3229142,
+	  	    lng: -122.005926
 	  	}
 	  },
 	  {
-	  	name: 'Mandarin Gourmet Cupertino',
+	  	name: 'Yeung Shing Restaurant',
 	  	category: 'Party',
 	  	coordinates: {
-	  		lat: 37.324742,
-	  	    lng: -122.032612
+	  		lat: 37.2620874,
+	  	    lng: -121.9627314
 	  	}
 	  },
 	  {
-	  	name: 'Fuji HuoShao & Dumpling',
+	  	name: 'Local Cafe',
 	  	category:'Alone',
 	  	coordinates: {
-	  		lat: 37.308790,
-	  		lng: -122.013532
+	  		lat: 37.3369847,
+	  		lng: -122.0403526
 	  	}
 	  },
 	  {
@@ -72,11 +72,11 @@ var locations = [{
 	  	}
 	  },
 	  {
-	  	name: 'Local Cafe',
+	  	name: 'Fuji Huoshao & Dumpling',
 	  	category:'Alone',
 	  	coordinates: {
-	  		lat: 37.336947,
-	  		lng: -122.040242
+	  		lat: 37.3085335,
+	  		lng: -122.0136769
 	  	}
 	  }
 ];
@@ -85,9 +85,9 @@ var foursquareLogin = {
         url: 'https://api.foursquare.com/v2/venues/search',
         dataType: 'json', 
         clientID: 'GHQH5GIMIKCEVVR44IYZRY5XE0LMP22QY2ETOWTHK32CZH3W',
-        clientSecret: '2AMVRG14H5WWHXGWLIZMBOPIMAPF50VQRNRO5JIBMWX45XOD', 
+        clientSecret: 'LAXWCAQJ3TEGOLXPKI1OAUNCGFTSILOXCLUSK3TCHAJ2BORW', 
         searchNear: 'Cupertino', 
-        requestDate: 20180120, 
+        requestDate: 20180122, 
         venueLink: "https://foursquare.com/v/" 
 };
 
@@ -101,7 +101,7 @@ var markers = [];
 function initMap() {
         var cupertino = {lat: 37.324126, lng: -122.041928};
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
+          zoom: 14,
           center: cupertino
         });
 
@@ -110,11 +110,12 @@ function initMap() {
         for(var i = 0; i < locations.length; i++){
         	var position = locations[i].coordinates;
         	var title = locations[i].name;
+        	console.log("title:"  + title);
         	marker = new google.maps.Marker({
         		map: map,
         		position: position,
-        		title: name,
-        		animation: google.maps.Animation.Drop
+        		title: title,
+        		animation: google.maps.Animation.Drop,
         	});
         //put the markers into an array
         markers.push(marker);
@@ -124,6 +125,7 @@ function initMap() {
 
         //event listener for the marker when it is clicked
         marker.addListener('click', function(){
+        	console.log(this);
         	populateInfoWindow(this, largeInfowindow);
         });
 
@@ -132,39 +134,42 @@ function initMap() {
       }
 
      //function for populating the inforWindow when the marker is clicked
-     function populateInfoWindow(marker, largeInforWindow){
+     function populateInfoWindow(marker, largeInfowindow){
      	var venue;
      	var windowsPop;
+     	console.log("marker: " + marker)
      	$.ajax({
      		url: foursquareLogin.url,
      		dataType: foursquareLogin.dataType,
      		data: {
-     			client_Id: foursquareLogin.clientID,
-     			client_Secret: foursquareLogin.clientSecret,
+     			client_id: foursquareLogin.clientID,
+     			client_secret: foursquareLogin.clientSecret,
      			query: marker.title,
      			near: foursquareLogin.searchNear,
      			v: foursquareLogin.requestDate
      		},
      		success: function(data){
      			venue = data.response.venues[0];
+
      			if(venue === null){
      				windowsPop = "<div class = 'name'> No Venues, please try again!</div>";
+     				largeInfowindow.setContent(windowsPop);
      			}else{
-     				inforWindow.setContent("<div class='name'>" + "Name: " + "<span class='info'>" + marker.title + "</span></div>" +
+     				largeInfowindow.setContent("<div class='name'>" + "Name: " + "<span class='info'>" + marker.title + "</span></div>" +
                                 "<div class='address'>" + "Location: " + "<span class='info'>" + venue.location.formattedAddress[0] + "</span></div>" +
                                 "<div class='foursquareInfo'>" + "Foursquare info: " + "<a href='" + foursquareLogin.venueLink + venue.id + "'>" + "Link" + "</a></div>");
      			}
 
-                largeInfowindow.setContent(windowsPop);
+               
      			marker.setAnimation(google.maps.Animation.BOUNCE);
      			setTimeout(function(){
      				marker.setAnimation(null);
      			}, 1800);
 
-                largeInfoWindow.open(map, marker);
+                largeInfowindow.open(map, marker);
 
-                largeInfoWindow.addListener('closeclick', function(){
-                	largeInfoWindow.setMarker = null;
+                largeInfowindow.addListener('closeclick', function(){
+                	largeInfowindow.setMarker = null;
                 });
      		},
      		error: function(){
